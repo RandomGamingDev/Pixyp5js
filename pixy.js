40d455992ae938dac8ce06874febdc8b2867f645
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2023-02-14 18:26:18 -0500

    Update pixy.js

--- pixy.js
+++ pixy.js
@@ -3,29 +3,24 @@ class Pixy {
     this.off = off;
     this.size = size;
     this.res = res;
-    this.pixels = [];
-    for (let i = 0; i < res[0]; i++) {
-      this.pixels.push([]);
-      for (let j = 0; j < res[1]; j++)
-        this.pixels[i].push(undefined);
-    }
+    this.img = createImage(res[0], res[1]);
+  }
+  
+  loadPixels() {
+    this.img.loadPixels();
+  }
+  
+  setPixel(loc, col) {
+    this.img.set(loc[0], loc[1], col);
+    this.img.updatePixels();
   }
   
   display() {
     push();
-    for (let i = 0; i < this.res[0]; i++)
-      for (let j = 0; j < this.res[1]; j++) {
-        if (this.pixels[i][j] == undefined)
-          continue;
-        fill(this.pixels[i][j]);
-        let pixelSize = [this.size[0] / this.res[0], this.size[1] / this.res[1]];
-        rect(
-              this.off[0] + pixelSize[0] * i,
-              this.off[1] + pixelSize[1] * j,
-              pixelSize[0],
-              pixelSize[1]
-            );
-      }
+    {
+      noSmooth();
+      image(this.img, this.off[0], this.off[1], this.size[0], this.size[1]);
+    }
     pop();
   }
 }
